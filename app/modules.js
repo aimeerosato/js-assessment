666a2bd5f374c02c69d771c0827064c055b17320
Aimee Rosato <aimee.rosato@gmail.com>
2016-06-08 17:45:48 -0400

    modules challenge complete

--- app/modules.js
+++ app/modules.js
@@ -2,21 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-  //   var module = function(str1, str2) {
-  //     this.greeting = str1;
-  //     this.name = str2;
-  //   };
-  //   console.log(module);
-  //   module.prototype.sayIt = function() {
-  //     return this.greeting + ", " + this.name;
-  //   };
-  //   console.log(module.prototype.sayIt);
-  // }
-      this.greeting = str1;
-      this.name = str2;
-      this.sayIt = function() {
-        return this.greeting + ", " + this.name;
+      var module = {
+        greeting: str1,
+        name: str2,
+        sayIt: function() {
+          return this.greeting + ", " + this.name;
+        }
       };
+
+      return module;
 }
 
 };
